518dbd36d8923a8f86aa101bfcb0c39a523bedf1
Carlos Tiago <carlostiagodesousa@gmail.com>
2024-06-22 17:21:57 -0300

    feat: inclusao da parte onde ira compor o conteudo da aplicaçcao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,26 @@ import {HeaderComponent} from "./core/header/header.component";
 import {NgOptimizedImage} from "@angular/common";
 import { MenuLateralComponent } from './core/menu/menu-lateral/menu-lateral.component';
 import { LogoComponent } from './core/logo/logo.component';
+import { MainComponent } from './core/main/main.component';
+import { BreadcrumbComponent } from './core/breadcrumb/breadcrumb.component';
+import {BreadcrumbModule} from "primeng/breadcrumb";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MenuLateralComponent,
-    LogoComponent
+    LogoComponent,
+    MainComponent,
+    BreadcrumbComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    Button,
-    NgOptimizedImage
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        Button,
+        NgOptimizedImage,
+        BreadcrumbModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/core/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,52 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {MenuItem} from "primeng/api";
+import {NavigationStart, Router, Event} from "@angular/router";
+
+@Component({
+  selector: 'app-breadcrumb',
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.css']
+})
+export class BreadcrumbComponent implements OnInit {
+
+    items: MenuItem[] | undefined;
+    home: MenuItem | undefined;
+
+    constructor(private router: Router) { }
+
+    ngOnInit(): void {
+        this.items = [{label: 'Dashboard'} ];
+        this.home = { icon: 'pi pi-home', routerLink: '/' };
+
+        this.router.events.subscribe((e: Event) => {
+            if (e instanceof NavigationStart) {
+                this.montarURL(e.url);
+            }
+        });
+    }
+
+    montarURL(url: string) {
+        if (url === '/') {
+            url = 'Dashboard/';
+        }
+
+        const arr = url.split('/')
+            .filter(pt => pt !== '');
+
+        const newArr = [];
+
+        for (const p of arr) {
+            const regex = /\d/;
+
+            if (regex.test(p)) {
+                continue;
+            }
+
+            newArr.push({
+                label: p.charAt(0).toUpperCase() + p.slice(1)
+            });
+        }
+
+        this.items = newArr;
+    }
+}
